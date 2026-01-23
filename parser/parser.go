0c570d5235cd463923bf642f6f3346885b633c2f
Matthew Grayson <matthew.grayson@associates.cisa.dhs.gov>
2026-01-23 14:20:57 -0600

    feat(parser): parse integer literals and register INT prefix handler
    * Add parseIntegerLiteral using strconv.ParseInt with error recording
    * Register token.INT as a prefix parse function during parser initialization

--- parser/parser.go
+++ parser/parser.go
@@ -2,6 +2,8 @@ package parser
 
 import (
 	"fmt"
+	"strconv"
+
 	"monkey/ast"
 	"monkey/lexer"
 	"monkey/token"
@@ -43,6 +45,7 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	// read two tokens to set both curToken and peekToken
 	p.nextToken()
@@ -103,6 +106,19 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+	lit.Value = value
+	return lit
 }
 
 func (p *Parser) parseLetStatement() *ast.LetStatement {
